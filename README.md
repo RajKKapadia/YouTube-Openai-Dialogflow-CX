9c7a0c30e5a84b150c8179f3b93d3fcc1bef1e0e
RajKKapadia <raajforyou@gmail.com>
2022-12-24 15:27:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Connect OpenAI to Dialogflow CX
+Hey everyone, if you are looking for a connection between OpenAI and Dialogflow CX, then read more. You will learn how to connect your existing Dialogflow CX agent to openAI.
+
+# Youtube
+I have recorded a quick tutorial on this topic, you can watch it [here]().
+
+### Things you will need
+* Dialogflow CX agent
+    > some knowledge of Dialogflow CX as well
+* OpenAI account and API Key
+    > create an account [here](https://openai.com/)
+* NGROK for exposing our local server to internet for testing
+    > get it from [here](https://ngrok.com/)
+* Nodejs as a programing tool
+    > install it from [here](https://nodejs.org/en/download/)
+
+### How to use it
+* install all the required packages in the project folder
+    > use the command `npm install --save`
+* create a `.env` file in the folder and set two variables `PORT` and `OPENAI_API_KEY`
+* run the local server `npm run start`
+* start NGROK engine
+    > make sure the ports are same
+* set the NGROK url in Webhook section
+    > `YOUR NGRK URL/dialogflow` it should be something like this
+* test the connection
+
+### Deploy
+* You can deploy this functionality on anywhere, for a free resource watch [this](https://youtu.be/0BeYenl5BqQ) video.
+
+# About me
+I am `Raj Kapadia`, I am passionate about `AI/ML/DL` and their use in different domains, I also love to build `chatbots` using `Google Dialogflow ES/CX`, I have backend development experience with Python[Flask], and NodeJS[Express] For any work, you can reach out to me at...
+
+* [LinkedIn](https://www.linkedin.com/in/rajkkapadia/)
+* [Fiverr](https://www.fiverr.com/rajkkapadia​)
+* [Upwork](https://www.upwork.com/freelancers/~0176aeacfcff7f1fc2)
+* [Youtube](https://www.youtube.com/channel/UCOT01XvBSj12xQsANtTeAcQ)
---
+++ index.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
+
+const webApp = express();
+
+webApp.use(express.urlencoded({
+    extended: true
+}));
+webApp.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const textGeneration = async (prompt) => {
+
+    try {
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `Human: ${prompt}\nAI: `,
+            temperature: 0.9,
+            max_tokens: 500,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0.6,
+            stop: ['Human:', 'AI:']
+        });
+
+        return {
+            status: 1,
+            response: `${response.data.choices[0].text}`
+        };
+    } catch (error) {
+        return {
+            status: 0,
+            response: ''
+        };
+    }
+};
+
+
+const formatResponseForDialogflow = (texts, sessionInfo, targetFlow, targetPage) => {
+
+    messages = []
+
+    texts.forEach(text => {
+        messages.push(
+            {
+                text: {
+                    text: [text],
+                    redactedText: [text]
+                },
+                responseType: 'HANDLER_PROMPT',
+                source: 'VIRTUAL_AGENT'
+            }
+        );
+    });
+
+    let responseData = {
+        fulfillment_response: {
+            messages: messages
+        }
+    };
+
+    if (sessionInfo !== '') {
+        responseData['sessionInfo'] = sessionInfo;
+    }
+
+    if (targetFlow !== '') {
+        responseData['targetFlow'] = targetFlow;
+    }
+
+    if (targetPage !== '') {
+        responseData['targetPage'] = targetPage;
+    }
+
+    return responseData
+};
+
+const getErrorMessage = () => {
+
+    return formatResponseForDialogflow(
+        [
+            'We are facing a technical issue.',
+            'Please try after sometimes or contact the XYZ restaurant.'
+        ],
+        '',
+        '',
+        ''
+    );
+};
+
+webApp.post('/dialogflow', async (req, res) => {
+
+    let tag = req.body.fulfillmentInfo.tag;
+    let query = req.body.text;
+
+    console.log('A new request came...');
+    console.log(tag);
+    console.log(new Date())
+
+    if (tag === 'sampleResponse') {
+        let result = await textGeneration(query);
+        if (result.status == 1) {
+            res.send(formatResponseForDialogflow(
+                [
+                    result.response
+                ],
+                '',
+                '',
+                ''
+            ));
+        } else {
+            res.send(getErrorMessage());
+        }
+
+    } else {
+        res.send(
+            formatResponseForDialogflow(
+                [
+                    'This is from the webhook.',
+                    'There is no tag set for this request.'
+                ],
+                '',
+                '',
+                ''
+            )
+        );
+    }
+});
+
+webApp.listen(PORT, () => {
+    console.log(`Server is up and running at ${PORT}`);
+});
